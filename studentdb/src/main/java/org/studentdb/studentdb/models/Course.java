6a2cbd3bf740d734b7be26cc2a7add8781dfb132
chrv81 <vochris81@gmail.com>
2026-06-07 22:41:11 -0400

    Add getters for each

--- studentdb/src/main/java/org/studentdb/studentdb/models/Course.java
+++ studentdb/src/main/java/org/studentdb/studentdb/models/Course.java
@@ -19,4 +19,32 @@ public class Course {
 
     @Column(nullable = false)
     private BigDecimal cost;
+
+    public Course() {}
+
+    // Constructor to receive the first name and last name
+    public Course(String courseCode, String courseName, BigDecimal cost) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.cost = cost;
+    }
+
+    // --- GETTERS ---
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    // --- SETTERS ---
 }
--- studentdb/src/main/java/org/studentdb/studentdb/models/Student.java
+++ studentdb/src/main/java/org/studentdb/studentdb/models/Student.java
@@ -18,8 +18,41 @@ public class Student {
     private String lastName;
 
     @Column(nullable = false)
-    private int gradeLevel;
+    private Integer gradeLevel;
 
     @Column(nullable = false)
     private BigDecimal balance;
+
+    public Student() {}
+
+    // Constructor to receive the first name and last name
+    public Student(String firstName, String lastName, Integer gradeLevel, BigDecimal balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradeLevel = gradeLevel;
+        this.balance = balance;
+    }
+
+    // --- GETTERS ---
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    // --- SETTERS ---
 }
